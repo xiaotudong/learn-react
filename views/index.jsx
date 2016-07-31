1dcdfb61b431a13b23d948a6c38532d6bc2f1296
dingqian <302965061@qq.com>
2016-07-31 18:15:40 +0800

    study Props

--- views/index.jsx
+++ views/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 export default class TodoBox extends React.Component {
     render() {
         return (
@@ -10,15 +11,33 @@ export default class TodoBox extends React.Component {
         );
     }
 }
+
 class TodoList extends React.Component {
     render() {
         return (
             <div className="todoList">
-                I am a TodoList.
+                <table style={{border: "2px solid black"}}>
+                    <tbody>
+                    <Todo title="Shopping">Milk</Todo>
+                    <Todo title="Hair cut">13:00</Todo>
+                    </tbody>
+                </table>
             </div>
         );
     }
 }
+
+class Todo extends React.Component {
+    render() {
+        return (
+            <tr>
+                <td style={{border: "1px solid black"}}>{this.props.title}</td>
+                <td style={{border: "1px solid black"}}>{this.props.children}</td>
+            </tr>
+        );
+    }
+}
+
 class TodoForm extends React.Component {
     render() {
         return (
